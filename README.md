214761adb5821aa42c5179de734e02fc91535feb
John Harrison <jwh0051@auburn.edu>
2016-08-27 17:54:51 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# minecraft-server
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'minecraft-server'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures minecraft-server'
+long_description 'Installs/Configures minecraft-server'
+version '0.1.0'
